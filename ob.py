e5ff2374e5c3e440ebc8764d08729b0b696e6a93
Ivan Pribylov <morkkrigare@gmail.com>
2020-06-12 17:26:22 +0300

    Параметры объектов

---
+++ ob.py
@@ -0,0 +1,25 @@
+from parameters import display
+
+
+class Object:
+    def __init__(self, x, y, width, image, speed):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.image = image
+        self.speed = speed
+
+    def move(self):
+        if self.x >= -self.width:
+            display.blit(self.image, (self.x, self.y))
+            self.x -= self.speed
+            return True
+        else:
+            return False
+
+    def return_self(self, radius, y, width, image):
+        self.x = radius
+        self.y = y
+        self.width = width
+        self.image = image
+        display.blit(self.image, (self.x, self.y))
