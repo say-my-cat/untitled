180fd2ef95b25de5efbe4a2a9e5418e2b0d29f9f
Ivan Pribylov <morkkrigare@gmail.com>
2020-06-12 17:23:30 +0300

    Кнопки на главном меню

---
+++ button.py
@@ -0,0 +1,30 @@
+from sounds import *
+from effects import *
+from settings import display
+
+
+class Button:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.inactive_clr = (13, 162, 58)
+        self.active_clr = (23, 204, 58)
+
+    def draw(self, x, y, message, action=None, font_size=30):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if x < mouse[0] < x + self.width and y < mouse[1] < y + self.height:
+            pygame.draw.rect(display, self.active_clr, (x, y, self.width, self.height))
+            if click[0] == 1:
+                pygame.mixer.Sound.play(button_sound)
+                pygame.time.delay(300)
+                if action is not None:
+                    if action == quit:
+                        pygame.quit()
+                        quit()
+                    else:
+                        action()
+        else:
+            pygame.draw.rect(display, self.inactive_clr, (x, y, self.width, self.height))
+        print_text(message=message, x=x+10, y=y+10)
