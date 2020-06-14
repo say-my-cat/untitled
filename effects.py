03b36d48b61346f68c0455bb436c63ea01d633c0
say-my-cat <62022024+say-my-cat@users.noreply.github.com>
2020-06-14 21:20:31 +0300

    Update effects.py

--- effects.py
+++ effects.py
@@ -6,7 +6,7 @@ input_text = '|'
 in_tic = 30
 
 
-def print_text(message, x, y, front_color=(0, 0, 0), front_type='fon/PingPong.ttf', front_size=30):
+def print_text(message, x, y, front_color=(255, 234, 167), front_type='fon/PingPong.ttf', front_size=30):
     front_type = pygame.font.Font(front_type, front_size)
     text = front_type.render(message, True, front_color)
     p.display.blit(text, (x, y))
